563e23e6c0e1b31aaaeaf933ef1b7043f6a12f0a
Chaya Berezin <chayushb@gmail.com>
2011-12-11 15:16:49 +0200

    first shoot of UDPClient

---
+++ src/UDPComm/UDPClient.java
@@ -0,0 +1,56 @@
+package UDPComm;
+
+import java.io.*;
+import java.net.*;
+
+class UDPClient
+{
+	
+	private InetAddress 	serverIPAddress;
+	private int				serverPort;
+	private DatagramSocket 	clientSocket;
+	
+	
+	public UDPClient(String IPAddr, int port) throws UnknownHostException, SocketException {
+		serverIPAddress = InetAddress.getByName(IPAddr);
+		serverPort		= port;
+		clientSocket 	= new DatagramSocket();
+	}
+	
+	
+	public boolean sendMessage(String message) throws IOException {
+		byte[] sendData = new byte[1024];
+		sendData = message.getBytes();
+		DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, serverIPAddress, serverPort);
+		try {
+			clientSocket.send(sendPacket);
+		}
+		catch(Exception exp) {
+			// log error
+			return false;
+		}
+		return true;
+	}
+	
+  /** public static void main(String args[]) throws Exception
+   {
+      BufferedReader inFromUser = 
+      new BufferedReader(new InputStreamReader(System.in));
+      DatagramSocket clientSocket = new DatagramSocket();
+      InetAddress IPAddress = InetAddress.getByName("");
+      byte[] sendData = new byte[1024];
+      byte[] receiveData = new byte[1024];
+      String sentence = inFromUser.readLine();
+      sendData = sentence.getBytes();
+      DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
+      clientSocket.send(sendPacket);
+      DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+      clientSocket.receive(receivePacket);
+      String modifiedSentence = new String(receivePacket.getData());
+      System.out.println("FROM SERVER:" + modifiedSentence);
+      clientSocket.close();
+   }**/
+   
+   
+   
+}
